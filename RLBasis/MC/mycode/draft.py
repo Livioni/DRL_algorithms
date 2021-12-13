1a51d746f9ae8adad01815e391e079b7de28d082
Livioni <livion_i@icloud.com>
2021-12-13 22:23:32 +0800

    update PPO algorithm
    读懂了PPO截断算法代码

--- RLBasis/MC/mycode/draft.py
+++
@@ -1,2 +0,0 @@
-import numpy as np
-print(np.shape([[1,2,3],[4,5,6]]))
--- playground.py
+++ playground.py
@@ -1,18 +1,10 @@
-import gym
-from torch.distributions import normal
-name = "Pendulum-v1"
+import torch
 
-env = gym.make(name)
-env.reset()
-action = env.action_space.sample()
-print(env.action_space)
-print(env.observation_space)
-observation, reward, done, info = env.step(action)
-print(observation, reward, done, info)
-while True:
-    # action = normal.Normal(0,1).sample().item()
-	action = env.action_space.sample()
-	observation, reward, done, info = env.step(action)
-	# print(reward)
-	print(action)
-	env.render()
+t3=torch.tensor([[0.5317, 0.4683],
+        [0.5355, 0.4645],
+        [0.5545, 0.4455],
+        [0.5294, 0.4706],
+        [0.5418, 0.4582]])
+
+result=t3.gather(1,torch.tensor([[0],[1],[1],[1],[0]]))
+print(result)
